ea614ca5b1bf6dd087eb8034e0a6674ed592f5e8
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 02:50:09 +0700

    Test

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,11 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatefulWidget {
 
   @override
    State<StatefulWidget> createState() => new _LoginPageState();
 
+}
+
+enum FormType {
+
+  login,
+  register
 }
 
 
@@ -16,6 +24,8 @@ class _LoginPageState extends State<LoginPage>{
 
   String _email;
   String _password;
+  FormType _formType = FormType.login;
+
 
  bool validateAndSave(){
 
@@ -32,14 +42,38 @@ class _LoginPageState extends State<LoginPage>{
     return false;
   }
 
-  void validateAndSubmit(){
+  void validateAndSubmit() async {
 
     if(validateAndSave()){
+  try{
+    if(_formType == FormType.login){
+     FirebaseUser user =  await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+      print('Signed in: ${user.uid}');
+    }else{
+      FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+      print('Register user : ${user.uid}');
+    }
+  }
+    catch(e){
+    print('Error :  $e');
 
-      
-      
+  }
     }
 
+  }
+
+  void moveToRegister(){
+  formKey.currentState.reset();
+   setState(() {
+     _formType = FormType.register;
+   });
+  }
+
+  void moveToLogin(){
+
+   setState(() {
+     _formType = FormType.login;
+   });
   }
 
   @override
@@ -51,27 +85,61 @@ class _LoginPageState extends State<LoginPage>{
         body: new Container(
           padding: EdgeInsets.all(16.0),
           child: new Form(
+            key: formKey,
             child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                new TextFormField(
-                  decoration: new InputDecoration(labelText: 'Email' ),
-                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
-                  onSaved: (value) => _email = value,
-                  ),
-                  new TextFormField(
-                    decoration: new InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-                    onSaved: (value) => _password = value,
-                  ),
-                  new RaisedButton(
-                    child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
-                    onPressed: validateAndSubmit,
-                  )
-              ],
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: buildInputs() + buildSubmitButtons(),
             )
           )
           ));
+  }
+
+  List<Widget> buildInputs(){
+
+   return[
+
+     new TextFormField(
+       decoration: new InputDecoration(labelText: 'Email' ),
+       validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+       onSaved: (value) => _email = value,
+     ),
+     new TextFormField(
+       decoration: new InputDecoration(labelText: 'Password'),
+       obscureText: true,
+       validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+       onSaved: (value) => _password = value,
+     ),
+
+   ];
+
+  }
+
+  List<Widget> buildSubmitButtons(){
+   if(_formType == FormType.login) {
+     return [
+
+       new RaisedButton(
+         child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+         onPressed: validateAndSubmit,
+       ),
+       new FlatButton(
+         child: new Text('Sign up', style: new TextStyle(fontSize: 20.0)),
+         onPressed: moveToRegister,
+       )
+
+     ];
+   }else{
+     return [
+       new RaisedButton(
+         child: new Text('Sign up', style: new TextStyle(fontSize: 20.0)),
+         onPressed: validateAndSubmit,
+       ),
+       new FlatButton(
+         child: new Text('Have an account ? Login', style: new TextStyle(fontSize: 20.0)),
+         onPressed: moveToLogin,
+       )
+
+     ];
+   }
   }
 }
