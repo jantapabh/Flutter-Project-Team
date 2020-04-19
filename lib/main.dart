2de2ac1e1cf0468a56825cc25184dca7e1f3096c
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 03:15:07 +0700

    ADD COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget{
       theme: new ThemeData(
         primarySwatch: Colors.blue,
        ),
-       home: new LoginPage(auth: new Auth());
+       home: new LoginPage(auth: new Auth()),
     );
   }
 }
