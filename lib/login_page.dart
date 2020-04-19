5e21a7981dc638ec1f6c3508080202cfe68321b2
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 00:06:29 +0700

    Add LoginForm

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,12 +14,36 @@ class _LoginPageState extends State<LoginPage>{
   String _email;
   String _password;
 
-  @verride
+  void validateAndSave(){
+
+
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text('Flutter Login Demo'),
         ),
-        body: new Container(child: new Text('Hello World')));
+        body: new Container(
+          padding: EdgeInsets.all(16.0),
+          child: new Form(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: 'Email' ),),
+                  new TextFormField(
+                    decoration: new InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  new RaisedButton(
+                    child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+                    onPressed: validateAndSave,
+                  )
+              ],
+            )
+          )
+          ));
   }
 }
