136c559aa7ca4e7c2ee5d2717fb953502f2212ce
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-19 23:51:45 +0700

    Add Commit Login

---
+++ lib/login_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+
+  @override
+   State<StatefulWidget> createState() => new _LoginPageState();
+
+}
+
+
+
+class _LoginPageState extends State<LoginPage>{
+
+  String _email;
+  String _password;
+
+  @verride
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter Login Demo'),
+        ),
+        body: new Container(child: new Text('Hello World')));
+  }
+}
