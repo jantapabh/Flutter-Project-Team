b1262b232b118a00d8cb33ce9196306f24cf95d5
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 12:13:28 +0700

    ADD COMMIT WELCOME PAGE

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -11,29 +11,29 @@ class RootPage extends StatefulWidget {
   State<StatefulWidget> createState() => new _RootPageState();
 }
 
-enum AuthStatus{
-
-  notSignedIn,
-  signedIn
-}
+enum AuthStatus { notSignedIn, signedIn }
 
 class _RootPageState extends State<RootPage> {
+  AuthStatus authStatus = AuthStatus.notSignedIn;
 
-  AuthStatus _authStatus = AuthStatus.notSignedIn;
-
+  @override
+  void initState() {
+    super.initState();
+    widget.auth.currentUser().then((userId) {
+      setState(() {
+        authStatus = userId == null ?  AuthStatus.notSignedIn : AuthStatus.signedIn;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    switch(_authStatus){
-
+    switch (authStatus) {
       case AuthStatus.notSignedIn:
-       return new LoginPage(auth: widget.auth);
+        return new LoginPage(auth: widget.auth);
 
       case AuthStatus.signedIn:
-      return new Container(
-        child: new Text('Welcome')
-      );
+        return new Container(child: new Text('Welcome'));
     }
   }
 }
