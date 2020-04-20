f5a4ce27c115bf978d073357b36076baa018cd63
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 11:56:34 +0700

    ADD ROOT PAGE

---
+++ lib/root_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project/login_page.dart';
+import 'auth.dart';
+
+class RootPage extends StatefulWidget {
+  RootPage({this.auth});
+  final BaseAuth auth;
+
+  @override
+  State<StatefulWidget> createState() => new _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new LoginPage(auth: widget.auth);
+  }
+}
