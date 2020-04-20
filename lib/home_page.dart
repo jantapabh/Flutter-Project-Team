091303dfaf2328f9512a9715d7a7033bdd9edaf3
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-20 14:09:32 +0700

    Add commit HomePage and Use

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,42 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:project/auth.dart';
 import 'auth.dart';
 
+//ส่วน Homepage แสดงการ Login และเช็กการเข้าระบบ
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
 
   HomePage({this.auth, this.onSignedOut});
   final BaseAuth auth;
   final VoidCallback onSignedOut;
 
-   void _signOut() async {
 
-     try {
-       await auth.signOut();
-       onSignedOut();
+  void _signOut() async {
+    try {
+      await auth.signOut();
+      onSignedOut();
+    } catch (e) {
+      print(e);
+    }
+  }
 
-     } catch(e) {
-       print(e);
-     }
-   }
+  //ส่วนแสดงผล
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Welcome'),
-        actions: <Widget>[
-          new FlatButton(
-        child: new Text('Logout', style: new TextStyle(fontSize: 32.0, color: Colors.white)),
-        onPressed: _signOut
-        )
-        ],
-      ),
-      body: new Container(
-        child: new Center(
-          child: new Text('Welcome', style: new TextStyle(fontSize: 32.0))
-          ),
-      )
-    );
+        appBar: new AppBar(
+          title: new Text('Welcome'),
+          actions: <Widget>[
+            new FlatButton(
+                child: new Text('Logout',
+                    style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+                onPressed: _signOut)
+          ],
+        ),
+        body: new Container(
+          child: new Center(
+              child: new Text('Welcome', style: new TextStyle(fontSize: 32.0))),
+        ));
   }
 }
